c692846802e59a24b57c5f973561dc813d9fba22
diasqazaqbro <adilovdiasdog2006@mail.ru>
2023-06-22 01:07:23 +0600

    commit

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,8 +1,6 @@
-import Spinner from '@/components/Spinner'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { ReactSortable } from 'react-sortablejs'
 
 export default function ProductForm({
 	_id,
@@ -20,7 +18,7 @@ export default function ProductForm({
 		assignedProperties || {}
 	)
 	const [price, setPrice] = useState(existingPrice || '')
-	const [images, setImages] = useState(existingImages || [])
+	const [images, setImages] = useState('')
 	const [goToProducts, setGoToProducts] = useState(false)
 	const [isUploading, setIsUploading] = useState(false)
 	const [categories, setCategories] = useState([])
@@ -60,15 +58,7 @@ export default function ProductForm({
 			for (const file of files) {
 				data.append('file', file)
 			}
-			const res = await axios.post('/api/upload', data)
-			setImages(oldImages => {
-				return [...oldImages, ...res.data.links]
-			})
-			setIsUploading(false)
 		}
-	}
-	function updateImagesOrder(images) {
-		setImages(images)
 	}
 	function setProductProp(propName, value) {
 		setProductProperties(prev => {
@@ -100,6 +90,13 @@ export default function ProductForm({
 				value={title}
 				onChange={ev => setTitle(ev.target.value)}
 			/>
+			<label>Фото</label>
+			<input
+				type='text'
+				placeholder='Ссылка на фотографию'
+				value={images}
+				onChange={ev => setImages(ev.target.value)}
+			/>
 			<label>Категория</label>
 			<select value={category} onChange={ev => setCategory(ev.target.value)}>
 				<option value=''>Нет Категории</option>
@@ -113,7 +110,7 @@ export default function ProductForm({
 			{propertiesToFill.length > 0 &&
 				propertiesToFill.map(p => (
 					<div key={p.name} className=''>
-						<label>{p.name[0].toUpperCase() + p.name.substring(1)}</label>
+						<label>{p.name[0] + p.name.substring(1)}</label>
 						<div>
 							<select
 								value={productProperties[p.name]}
@@ -128,47 +125,7 @@ export default function ProductForm({
 						</div>
 					</div>
 				))}
-			<label>Фото</label>
-			<div className='mb-2 flex flex-wrap gap-1'>
-				<ReactSortable
-					list={images}
-					className='flex flex-wrap gap-1'
-					setList={updateImagesOrder}
-				>
-					{!!images?.length &&
-						images.map(link => (
-							<div
-								key={link}
-								className='h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200'
-							>
-								<img src={link} alt='' className='rounded-lg' />
-							</div>
-						))}
-				</ReactSortable>
-				{isUploading && (
-					<div className='h-24 flex items-center'>
-						<Spinner />
-					</div>
-				)}
-				<label className='w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary'>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						fill='none'
-						viewBox='0 0 24 24'
-						strokeWidth={1.5}
-						stroke='currentColor'
-						className='w-6 h-6'
-					>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5'
-						/>
-					</svg>
-					<div>Добавить Фото</div>
-					<input type='file' onChange={uploadImages} className='hidden' />
-				</label>
-			</div>
+
 			<label>Описание</label>
 			<textarea
 				placeholder='Описание'
