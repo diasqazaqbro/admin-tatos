70515a5a08c995ff9ae2d7cc77ca6487dd511aa3
diasqazaqbro <adilovdiasdog2006@mail.ru>
2023-07-13 15:17:16 +0600

    fix: added env for email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-const adminEmails = ['diasqazaqbro@mail.ru']
+const adminEmails = [process.env.NEXTAUTH_SECRET]
 
 export const authOptions = {
 	secret: process.env.NEXTAUTH_SECRET,
